74e28e64a53de37cf66075964af42efe5c9fec42
Samuel Wabwana <samuel.wabwana@evokay.com>
2026-07-17 10:35:32 +0100

    adding timer

---
+++ app/components/timer-base.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="w-full max-w-100 aspect-video bg-green-600/40 rounded-2xl">
+        <div class="w-full h-full flex items-center justify-center">
+            <div class=" rounded-lg bg-green-600/70 p-4 w-25 time">
+                dssfsdf
+            </div>
+        </div>
+    </div>
+</template>
+<script setup lang="ts">
+import { $, Timer, createTimer } from 'animejs';
+
+let timer: Timer;
+onMounted(() => {
+    const [$time] = $('.time')
+    timer = createTimer({
+        duration: 2000,
+        alternate: true,
+        loop: true,
+        onUpdate: self => {
+            if ($time) {
+                return $time.innerHTML = self.iterationCurrentTime.toString();
+            }
+        }
+    })
+});
+</script>
---
+++ app/pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        <TimerBase />
+    </div>
+</template>
+<script setup lang="ts">
+
+</script>
